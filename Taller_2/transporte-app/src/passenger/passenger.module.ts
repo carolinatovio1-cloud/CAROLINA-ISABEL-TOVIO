e54013b94f237cb7ee72cac3e3b0dcd8a0eb7d7d
Carolina Isabel Tovio <carolinatovio1@gmail.com>
2025-10-01 23:25:33 -0500

    agregaré nuevamente taller_2

--- Taller_2/transporte-app/src/passenger/passenger.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PassengerController } from './passenger.controller';
-import { PassengerService } from './passenger.service';
-
-@Module({
-  controllers: [PassengerController],
-  providers: [PassengerService]
-})
-export class PassengerModule {}
